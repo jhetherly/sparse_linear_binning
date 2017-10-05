911aaa8050d6bb7ec0833dc86a46afeaa35b8594
Jeffrey Hetherly <jwh0118@me.com>
2017-10-04 21:07:09 -0400

    fixed setup.py

--- setup.py
+++ setup.py
@@ -60,7 +60,7 @@ if use_cython:
 else:
     ext_modules += [extensions]
 
-long_description = read('README.rst', 'CHANGES.txt')
+long_description = read('README.rst', 'CHANGELOG.md')
 
 setup(
     name="sparse_linear_binning",
